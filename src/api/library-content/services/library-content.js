60e121bb68e94377f31b6419b4beb7971f62e5bb
Jared <jared@thedelta.io>
2024-09-26 15:17:05 +0200

    upgrade strapi v5

--- src/api/library-content/services/library-content.js
+++ src/api/library-content/services/library-content.js
@@ -4,22 +4,25 @@ const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::library-content.library-content', ({ strapi }) => ({
   async incrementLikeCount(id) {
-    const entry = await strapi.entityService.findOne('api::library-content.library-content', id, {
-      fields: ['like_count'],
-    });
+    // const entry = await strapi.documents('api::library-content.library-content').findOne({
+    //   documentId: "__TODO__",
+    //   fields: ['like_count']
+    // });
 
-    if (!entry) {
-      throw new Error('Entry not found');
-    }
+    // if (!entry) {
+    //   throw new Error('Entry not found');
+    // }
 
-    const likeCount = entry.like_count && !isNaN(entry.like_count) ? parseInt(entry.like_count, 10) : 0;
+    // const likeCount = entry.like_count && !isNaN(entry.like_count) ? parseInt(entry.like_count, 10) : 0;
 
-    const updatedEntry = await strapi.entityService.update('api::library-content.library-content', id, {
-      data: {
-        like_count: likeCount + 1,
-      },
-    });
+    // const updatedEntry = await strapi.documents('api::library-content.library-content').update({
+    //   documentId: "__TODO__",
 
-    return updatedEntry;
+    //   data: {
+    //     like_count: likeCount + 1,
+    //   }
+    // });
+
+    // return updatedEntry;
   },
 }));
